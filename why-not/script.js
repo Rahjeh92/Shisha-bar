1354b6d2aadb54f7f11e734cf0597b30b0221c38
Zain <69877681+Rahjeh92@users.noreply.github.com>
2025-03-22 18:06:01 +0100

    Add files via upload

---
+++ why-not/script.js
@@ -0,0 +1,8 @@
+function toggleMenu(id) {
+    var menu = document.getElementById(id);
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
