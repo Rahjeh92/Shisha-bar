56482015c5d7671a559635a0d44ba0b2f020ef4b
Zain <69877681+Rahjeh92@users.noreply.github.com>
2025-03-22 18:07:36 +0100

    Delete why-not directory

--- why-not/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(id) {
-    var menu = document.getElementById(id);
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-}
