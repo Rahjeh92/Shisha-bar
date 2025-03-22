b54e30e4429f109d245a1df9d891898eff5a57d5
Zain <69877681+Rahjeh92@users.noreply.github.com>
2025-03-22 18:56:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function toggleMenu(id) {
     var menu = document.getElementById(id);
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
+
+    if (menu.style.maxHeight) {
+        menu.style.maxHeight = null; // Collapse this menu
     } else {
-        menu.style.display = "block";
+        menu.style.maxHeight = menu.scrollHeight + "px"; // Expand only this menu
     }
 }
