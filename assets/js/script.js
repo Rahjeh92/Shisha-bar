d2e303d6308888d57dd57f57dcdbe61a3cbf1945
Zain <69877681+Rahjeh92@users.noreply.github.com>
2025-03-23 17:01:24 +0100

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu(id) {
-    var menu = document.getElementById(id);
-
-    if (menu.style.maxHeight) {
-        menu.style.maxHeight = null; // Collapse this menu
-    } else {
-        menu.style.maxHeight = menu.scrollHeight + "px"; // Expand only this menu
-    }
-}
